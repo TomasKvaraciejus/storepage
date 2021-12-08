16ec111e97b99b768e1d9c13c531ade214681f2f
Matas-Samsonas <94130181+Matas-Samsonas@users.noreply.github.com>
2021-12-08 14:50:19 +0200

    Update storepage.c

--- storepage.c
+++ storepage.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 
 #define MENU_ITEM_AMOUNT 9
-#define MAX_STRING_TITLE_LENGTH 128
-#define MAX_STRING_DESCRIPTION_LENGTH 256
-#define MAX_STRING_DETAILS_LENGTH 128
+
 #define NEWLINE printf("\n");
 
 // ---- Menu and User Input ----
@@ -23,14 +21,18 @@
 // * - mandatory (User will be prompted to enter details when attempting to compile website without sufficient data)
 //		(User will be notified on currently empty mandatory fields with "*" character appended to name of field)
 //
+// ----------------
+//
 // Details will expand into separate menu accessed by index "menuIndex"
 // 0 - Back (Return to Main Menu)
 // 1 - Add New List Item to Top
-// 2 - Add New List Item to Bottom
-// 3 - Insert New List Item to Custom Index
+// 2 - Insert New List Item to Custom Index
 //		(User will be provided with current Details list and prompted to enter index)
+// 3 - Modify List Item 
 // 4 - Remove List Item
 //		(User will be provided with current Details list and prompted to enter index of list item to remove)
+//
+// ----------------
 //
 // Seller's Contacts will expand into separate menu accessed by index "menuIndex"
 //		(At least one field must contain contact information)
@@ -39,25 +41,43 @@
 // 2 - Add or Replace Email Address
 // 3 - Add or Replace Custom Contact Link
 //
-// ---- Website Compilation ----
+// ---- Website Compilation ---- 
 // Website will be compiled with highest index (8)
 // On successful compilation program will exit
+//
 
-// TODO:
+// ---- TODO: ----
 // Validate String Input (MAXCHAR)
 // Save User Input
 // Details Menu
 // Seller's Contacts Menu
 //
-// Compile Website Function
+// Compile Website Function 
+
+struct linkedList
+{
+	int maxStringLength;
+	int index;
+	char* s;
+	struct linkedList* nextListItem;
+};
+typedef struct linkedList linkedList;
 
 int validateIndexInput();
-char * validateStringInput(int maxLength);
+
+int addLinkedListItem(linkedList* l, char* s);
+
+int removeLinkedListItem(linkedList* l, int index);
+
+int insertLinkedListItem(linkedList* l, char *s, int index);
+
 int main()
 {
 	int menuIndex;
 	char* mainMenuOptions[MENU_ITEM_AMOUNT] = {"Item Name", "Image Source", "Item Price", "Shipping Cost", "Description", "More Information (Link)", "Details", "Seller's Contacts", "Compile Website and Exit"};
 	char mainMenuFields[MENU_ITEM_AMOUNT] = { 1, 0, 1, 0, 0, 0, 0, 1, 0}; // 0 - Empty, non-mandatory; 1 - Empty, mandatory; 2 - Non-empty
+
+	linkedList mainMenuValues[MENU_ITEM_AMOUNT];
 
 	char end = 0;
 	while (!end)
@@ -71,8 +91,85 @@ int main()
 
 		menuIndex = validateIndexInput();
 
+		if (menuIndex == 8)
+		{
+
+		}
+		else if (menuIndex == 6)
+		{
+
+		}
+		else if (menuIndex == 7)
+		{
+
+		}
+		else
+		{
+			printf("Enter New %s: ", mainMenuOptions[menuIndex]);
+			mainMenuValues[menuIndex].s = realloc(mainMenuValues[menuIndex].s, sizeof(char) * );
+		}
+
 		++end;
 	}
+}
+
+int addLinkedListItem(linkedList* l, char* s)
+{
+	int addValue = 0;
+
+	while (l->nextListItem == NULL)
+	{
+		l = l->nextListItem;
+	}
+
+	linkedList *newListItem;
+	newListItem->s = s;
+	newListItem->nextListItem = NULL;
+	newListItem->maxStringLength = l->maxStringLength;
+	l->nextListItem = newListItem;
+
+	return addValue;
+}
+
+int removeLinkedListItem(linkedList *l, int index)
+{
+	int removeValue = 0;
+
+	linkedList* prevListItem;
+
+	if (l->index > index)
+	{
+		++removeValue;
+	}
+	else
+	{
+		while (l->index != index)
+		{
+			if (l->nextListItem != NULL)
+			{
+				prevListItem = l;
+				l = l->nextListItem;
+			}
+			else
+			{
+				++removeValue;
+				break;
+			}
+		}
+
+		if (l->index == index)
+		{
+			prevListItem->nextListItem = l->nextListItem;
+			free(l);
+		}
+	}
+
+	return removeValue;
+}
+
+int insertLinkedListItem(linkedList *l, char *s, int index)
+{
+
 }
 
 int validateIndexInput()
@@ -89,24 +186,8 @@ int validateIndexInput()
 		putc(peekChar, stdin);
 		printf("err: incorrect input\nEnter index: ");
 	}
-
 	NEWLINE
 
 	return input;
-}
-char * validateStringInput(int maxLength)
-{
-    char * input = (char *) malloc(maxLength);
-    printf("Input string (character limit: %d): ", maxLength);
-    fgets(input, maxLength + 1, stdin);
-    if(getchar() != '\n')
-    {
-        printf("war: input exceeds character limit. Your input will be shortened in order to fit.\n");
-        while(getchar() != '\n')
-        {
-
-        }
-    }
-    return input;
 }
 
