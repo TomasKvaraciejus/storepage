d6e0b26c929aa5fbdeeaefc80290f6d9d9841297
Domas Nemanius <78321529+Domasen@users.noreply.github.com>
2021-12-27 19:56:16 +0200

    Add files via upload

--- storepage.c
+++ storepage.c
@@ -87,6 +87,8 @@ void compileAboutUs();
 
 void compileContacts();
 
+void createMainPage();
+
 int main()
 {
 	int menuIndex;
@@ -280,6 +282,7 @@ void compileWebsite(linkedList** mainMenuValues, linkedList** userContactsMenuVa
 
 	compileAboutUs();
 	compileContacts();
+	createMainPage();
 
 	fprintf
 	(outputFile,
@@ -804,3 +807,57 @@ int addLinkedListItemToTop(linkedList** l, char* s) //Adds a new element to the 
 	}
 	return 0;
 }
+
+void createMainPage(){
+    FILE *mainpage = NULL;
+
+    mainpage = fopen("mainpage.html", "w");
+
+
+    fprintf(mainpage,
+        "<!DOCTYPE html>\n"
+        "<html>\n"
+        "<head>\n"
+        "<link href=\"style.css\" rel=\"stylesheet\" />\n"
+        "<title>IrmaList</title>\n"
+        "</head>\n"
+        "<body>\n"
+        "<h1 style = \"margin-bottom:4px\"> World's Best Store - <span class=\"demo rainbow\">IrmaList</span> </h1>\n"
+        "<hr style = \"width:80%; text-align: center\">\n"
+        "<h3 style = \"text-align: center; margin-top: 0px; margin-bottom: 0px\"> <a href = \"#\">main page</a> | <a href = \"aboutus.html\">about us</a> | <a href = \"contacts.html\"> contact us </a></h3>\n"
+        "<hr style = \"width: 80%; text-align: center\">\n"
+        "<h3 style = \"margin-bottom:12px;margin-left: 24px\"> Most viewed posts: </h3>\n"
+        "<table>\n"
+        "<tr>\n"
+        "<td style = \"padding-left:12px; padding-right:12px; border:1px solid black; width:100px\"> <a href = \" \"> <img src = \"Images/powder.jpg\" style = \"object-fit:scale-down\" width = \"250\" height = \"150\"> </a> </td>\n"
+        "<td style = \"padding-left:12px; padding-right:12px; border:1px solid black; width:100px\"> <a href = \" \"> <img src = \"Images/stonks.png\" style = \"object-fit:scale-down\" width = \"250\" height = \"150\"> </a> </td>\n"
+        "<td style = \"padding-left:12px; padding-right:12px; border:1px solid black; width:100px\"> <a href =\" \"> <img src = \"Images/plus.png\" style = \"object-fit:scale-down\" width = \"250\" height = \"150\"> </a> </td>\n"
+        "</tr>\n"
+        "<tr>\n"
+        "<td style = \"padding-left:12px\"> <a href = \" \"> null </a> </td>\n"
+        "<td style = \"padding-left:12px\"> <a href = \" \"> null </a> </td>\n"
+        "<td style = \"padding-left:12px\"> <a href = \" \"> browse more... </a> </td>\n"
+        "</tr>\n"
+        "</table> \n"
+        "<col style = \"width: 12px\"/>\n"
+        "<h3 style = \"margin-top:48px;margin-bottom:12px;margin-left: 24px;\"> Most recent posts: </h3>\n"
+        "<table style = \"table-layout:fixed\">\n"
+        "<tr>\n"
+        "<td style = \"padding-left:12px; padding-right:12px; border:1px solid black; width:100px\"> <a href = \" \"> <img src = \"Images/stonks.png\" style = \"object-fit:scale-down\" width = \"250\" height = \"150\"> </a> </td>\n"
+        "<td style = \"padding-left:12px; padding-right:12px; border:1px solid black; width:100px\"> <a href = \" \"> <img src = \"Images/toplawyer.jpg\" style = \"object-fit:scale-down\" width = \"250\" height = \"150\"> </a> </td>\n"
+        "<td style = \"padding-left:12px; padding-right:12px; border:1px solid black; width:100px\"> <a href = \" \"> <img src = \"Images/plus.png\" style = \"object-fit:scale-down\" width = \"250\" height = \"150\"> </a> </td>\n"
+        "</tr>\n"
+        "<tr>\n"
+        "<td style = \"padding-left:12px\"> <a href = \" \"> null </a> </td>\n"
+        "<td style = \"padding-left:12px\"> <a href = \" \"> null </a> </td>\n"
+        "<td style = \"padding-left:12px\"> <a href = \" \"> browse more... </a> </td>\n"
+        "</tr>\n"
+        "</table> \n"
+        "<p id=\"copyright\">&copy; 2021 Adomas, Tomas, Matas, Domas</p>\n"
+        "</body>\n"
+        "</html>\n"
+    );
+
+    fclose(mainpage);
+}
+
